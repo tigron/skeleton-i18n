364627d436d4c5a76de6d57dd98f259ef85e5af4
Roan Buysse <roan.buysse@tigron.be>
2025-07-03 11:57:38 +0200

    Added a new translator service for Deepl

--- lib/Skeleton/I18n/Translator/Service.php
+++ lib/Skeleton/I18n/Translator/Service.php
@@ -6,8 +6,7 @@
  */
 namespace Skeleton\I18n\Translator;
 
-abstract class Service extends \GuzzleHttp\Client {
-
+abstract class Service {
 	/**
 	 * Returns a translation entry object with the destination (translation)
 	 *
@@ -15,4 +14,20 @@ abstract class Service extends \GuzzleHttp\Client {
 	 * @return \Skeleton\I18n\Translation\Entry
 	 */
 	abstract public function translate(\Skeleton\I18n\Translation\Entry $entry): \Skeleton\I18n\Translation\Entry;
+
+	/**
+	 * Set the source language
+	 *
+	 * @param \Language $language
+	 * @return void
+	 */
+	abstract public function set_source_language(\Language $language): void;
+
+	/**
+	 * Set the target language
+	 *
+	 * @param \Language $language
+	 * @return void
+	 */
+	abstract public function set_target_language(\Language $language): void;
 }
--- lib/Skeleton/I18n/Translator/Service/Deepl.php
+++ lib/Skeleton/I18n/Translator/Service/Deepl.php
@@ -1,19 +1,33 @@
 <?php
 /**
- * Service Tas
+ * Service Deepl
  *
  * @author Roan Buysse <roan@tigron.be>
  */
 namespace Skeleton\I18n\Translator\Service;
 
-class Tas extends \Skeleton\I18n\Translator\Service {
+class Deepl extends \Skeleton\I18n\Translator\Service {
 
 	/**
 	 * Endpoint
 	 *
 	 * @var $endpoint
 	 */
-	private $endpoint = 'https://655.mtis.workers.dev';
+	private $endpoint = 'https://api.deepl.com';
+
+	/**
+	 * Endpoint
+	 *
+	 * @var $endpoint
+	 */
+	private $api_key = '';
+
+	/**
+	 * Api Version
+	 *
+	 * @var $api_version
+	 */
+	private $api_version = 'v2';
 
 	/**
 	 * Source Language
@@ -57,12 +71,13 @@ class Tas extends \Skeleton\I18n\Translator\Service {
 	 *
 	 * @return void
 	 */
-	private function init_client() :void {
+	private function init_client(): void {
 		if (isset($this->client) === false) {
 			$config = [
 				'base_uri' => $this->endpoint,
 				'headers' => [
-					'Content-Type' => 'application/json; charset=UTF8'
+					'Content-Type' => 'application/json; charset=UTF8',
+					'Authorization' => 'DeepL-Auth-Key ' . $this->api_key
 				],
 			];
 			$this->client = new \GuzzleHttp\Client($config);
@@ -76,8 +91,15 @@ class Tas extends \Skeleton\I18n\Translator\Service {
 	 * @return \Skeleton\I18n\Translation\Entry
 	 */
 	public function translate(\Skeleton\I18n\Translation\Entry $entry): \Skeleton\I18n\Translation\Entry {
+		$this->init_client();
+
+		if (empty($this->api_key) === true) {
+			throw new \Exception("An Api key needs to be set to use the DeepL translator service");
+		}
+
 		$result = $this->translation_request($entry->source);
-		$entry->set($result->response->translated_text, true);
+		$translation = array_shift($result->translations);
+		$entry->set($translation->text, true);
 		return $entry;
 	}
 
@@ -103,7 +125,7 @@ class Tas extends \Skeleton\I18n\Translator\Service {
 	}
 
 	/**
-	 * Set the edpoint for  a service
+	 * Set the endpoint for a service
 	 *
 	 * @param \Language $language
 	 * @return void
@@ -113,22 +135,40 @@ class Tas extends \Skeleton\I18n\Translator\Service {
 	}
 
 	/**
+	 * Set the api_version for a service
+	 *
+	 * @param \Language $version
+	 * @return void
+	 */
+	public function set_api_version(string $version): void {
+		$this->api_version = $version;
+	}
+
+
+	/**
+	 * Set the api_key for a service
+	 *
+	 * @param \String $api_key
+	 * @return void
+	 */
+	public function set_api_key(string $api_key): void {
+		$this->api_key = $api_key;
+	}
+
+	/**
 	 * Translation request to the api
 	 *
 	 * @param string $text
 	 * @return \stdClass
 	 */
 	private function translation_request(string $text): \stdClass {
-		$this->init_client();
+		$options['form_params'] = [
+			'text' => $text,
+			'source_lang' => $this->source_lang,
+			'target_lang' => $this->target_lang,
+		];
 
-		$result = $this->client->request(
-			'GET',
-			'translate?text=' . $text
-			. '&source_lang=' . $this->source_lang
-			. '&target_lang=' .  $this->target_lang,
-		);
-		// We need to wait 0.25 secs after every call to  avoid being rate limited.  (DO NOT REMOVE CAN LEAD TO PERMANENT BLOCK)
-		usleep(250000);
+		$result = $this->client->request('POST',  $this->api_version . '/translate', $options);
 		return json_decode($result->getBody());
 	}
 }
